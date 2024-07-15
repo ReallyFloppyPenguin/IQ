ce09aadc3d23bcbe7984f1ff2e44c7abbda62940
gv-coder <72353581+gv-coder@users.noreply.github.com>
2024-07-15 18:38:00 +0100

    Fixed files

--- data.py
+++
@@ -1,5 +0,0 @@
-import pyttsx3 as voice
-
-name = "IQ"
-age = "0"
-fav_food = "pizza"
--- utils/errors.py
+++
@@ -1,5 +0,0 @@
-class UnevenDataErr(SyntaxError):
-    """Error for when the data is not even"""
-    def __init__(self, *args: object) -> None:
-        
-        super().__init__(*args)
